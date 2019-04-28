3a374041243320c83995a1468493fdb3881f0d11
Leandro Sanches <leandro.sanches@emi.bz>
2019-04-28 18:49:39 -0300

    [FIX]: cat was not being found during the installation proccess, so it was removed

--- install/install.sh
+++ install/install.sh
@@ -1,8 +1,11 @@
-rm -rf /docker-install/i686-elf-tools-linux/
-unzip /docker-install/i686-elf-tools-linux.zip -d /docker-install/i686-elf-tools-linux
-
-if cat ~/.bashrc | grep "i686-elf"; then
+if [ls /install | grep "i686-elf-tools-linux.zip"]; then
   echo "OK";
 else
-  echo "export PATH=/docker-install/i686-elf-tools-linux/bin:$PATH" >> ~/.bashrc
+  wget -P /docker-install http://nimbleking.com/shiro/downloads/i686-elf-tools-linux.zip
 fi
+
+rm -rf /docker-install/i686-elf-tools-linux/
+
+unzip /docker-install/i686-elf-tools-linux.zip -d /docker-install/i686-elf-tools-linux
+
+echo "export PATH=/docker-install/i686-elf-tools-linux/bin:$PATH" >> ~/.bashrc
